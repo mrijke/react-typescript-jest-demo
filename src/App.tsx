91dc224249ca97804991368edd0af66dd887fd80
Maarten Rijke <maarten.rijke@securitymatters.eu>
2017-05-19 12:28:26 +0200

    More extensive example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  )
+interface IAppState {
+  counter: number;
+}
+
+export default class App extends React.Component<{}, IAppState> {
+
+  constructor() {
+    super();
+    this.state = {counter: 0};
+  }
+
+  private handleIncrementCounterClick = () => {
+    this.setState({ counter: this.state.counter+1 });
+  }
+
+  public render() {
+    return (
+      <div>
+        <p>Hello World</p>
+        <p>This thing has been clicked {this.state.counter} many times! So much.</p>
+        <button onClick={this.handleIncrementCounterClick}>Click me!</button>
+      </div>
+    )
+  }
 }
